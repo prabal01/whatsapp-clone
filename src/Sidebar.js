cc8cd5293ee8406531715e969630f6d5236ad860
Prabal <sprabal01@gmail.com>
2021-01-18 17:35:53 +0530

    intial commit

---
+++ src/Sidebar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Sidebar.css"
+
+function Sidebar(){
+ return (
+ <div className="sidebar">
+
+     <div className="sidebar__header">
+
+     </div>
+     <div className="sidebar__search">
+
+     </div>
+     <div className="sidebar__chats">
+         
+         </div>
+ </div>
+ )
+}
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCN_WpWMgATb4mCbfXQ4d-q3U1ti2PtbXI",
+    authDomain: "whatsapp-colne-869b7.firebaseapp.com",
+    projectId: "whatsapp-colne-869b7",
+    storageBucket: "whatsapp-colne-869b7.appspot.com",
+    messagingSenderId: "245299196607",
+    appId: "1:245299196607:web:2345db4ca1f696a19ccc0a",
+    measurementId: "G-79WDQEE588"
+  };
