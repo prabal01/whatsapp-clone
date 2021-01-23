b97898491b8d21aa198ea439a096077792a64eb9
Prabal <sprabal01@gmail.com>
2021-01-23 13:31:27 +0530

    added sidebar,sidebar icons using material ui icons and related css

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,20 +1,38 @@
 import React from "react";
 import "./Sidebar.css"
+import { Avatar, IconButton } from "@material-ui/core"
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+function Sidebar() {
+    return (
+        <div className="sidebar">
 
-function Sidebar(){
- return (
- <div className="sidebar">
+            <div className="sidebar__header">
+                <IconButton>
+                    <Avatar />
+                </IconButton>
+                <div className="sidebar__header__right">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
 
-     <div className="sidebar__header">
+                    {/* <MoreVertIcon /> */}
+                </div>
+            </div>
+            <div className="sidebar__search">
 
-     </div>
-     <div className="sidebar__search">
+            </div>
+            <div className="sidebar__chats">
 
-     </div>
-     <div className="sidebar__chats">
-         
-         </div>
- </div>
- )
+            </div>
+        </div>
+    )
 }
 export default Sidebar
